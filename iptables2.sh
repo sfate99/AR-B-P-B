8568fb387befbc969bb75c9ee2b421beb8dca74a
stack zhao <zyh001@users.noreply.github.com>
2017-12-14 09:27:53 +0800

    适配多种系统

--- iptables2.sh
+++ iptables2.sh
@@ -1,4 +1,34 @@
-PATH=/sbin:/usr/sbin:/bin:/usr/bin
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#Check Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+#Check OS
+if [ -n "$(grep 'Aliyun Linux release' /etc/issue)" -o -e /etc/redhat-release ];then
+OS=CentOS
+[ -n "$(grep ' 7\.' /etc/redhat-release)" ] && CentOS_RHEL_version=7
+[ -n "$(grep ' 6\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release6 15' /etc/issue)" ] && CentOS_RHEL_version=6
+[ -n "$(grep ' 5\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release5' /etc/issue)" ] && CentOS_RHEL_version=5
+elif [ -n "$(grep 'Amazon Linux AMI release' /etc/issue)" -o -e /etc/system-release ];then
+OS=CentOS
+CentOS_RHEL_version=6
+elif [ -n "$(grep bian /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Debian' ];then
+OS=Debian
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
+elif [ -n "$(grep Deepin /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Deepin' ];then
+OS=Debian
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
+elif [ -n "$(grep Ubuntu /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'Ubuntu' -o -n "$(grep 'Linux Mint' /etc/issue)" ];then
+OS=Ubuntu
+[ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
+Ubuntu_version=$(lsb_release -sr | awk -F. '{print $1}')
+[ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_version=16
+else
+echo "Does not support this OS, Please contact the author! "
+kill -9 $$
+fi
+#定义变量
 SSH=22
 FTP=20,21
 DNS=53
@@ -13,9 +43,9 @@ NET_BIOS=135,137,138,139,445
 DHCP=67,68
 initialize() 
 {
-	iptables -F # テーブル初期化
-	iptables -X # チェーンを削除
-	iptables -Z # パケットカウンタ・バイトカウンタをクリア
+	iptables -F
+	iptables -X
+	iptables -Z 
 	iptables -P INPUT   ACCEPT
 	iptables -P OUTPUT  ACCEPT
 	iptables -P FORWARD ACCEPT
@@ -27,13 +57,9 @@ finailize()
 	return 0
 	return 1
 }
-if [ "$1" == "dev" ]
-then
-	iptables() { echo "iptables $@"; }
-	finailize() { echo "finailize"; }
-fi
+setiptables(){
 initialize
-iptables -P INPUT   DROP # すべてDROP。すべての穴をふさいでから必要なポートを空けていくのが良い。
+iptables -P INPUT   DROP 
 iptables -P OUTPUT  ACCEPT
 iptables -P FORWARD DROP
 iptables -A INPUT -i lo -j ACCEPT # SELF -> SELF
@@ -135,13 +161,49 @@ then
 fi
 iptables -A INPUT  -j LOG --log-prefix "drop: "
 iptables -A INPUT  -j DROP
+}
+if [[ ${OS} =~ ^Ubuntu$|^Debian$ ]];then
+	iptables-restore < /etc/iptables.up.rules
+        setiptables
+	iptables-save > /etc/iptables.up.rules
+fi
 
+if [[ ${OS} == CentOS ]];then
+	if [[ $CentOS_RHEL_version == 7 ]];then
+		iptables-restore < /etc/iptables.up.rules
+		setiptables
+		iptables-save > /etc/iptables.up.rules
+	else
+                setiptables
+		/etc/init.d/iptables save
+		/etc/init.d/iptables restart
+	fi
+fi
 echo "在30秒内，iptables配置将被自动重置。"
 echo "请尝试建立新的SSH连接测试！"
-echo "如果没有问题，请输入jdkskq完成配置。"
+echo "如果没有问题，请输入jdkskq完成配置，输入其它值也将重置。"
 read -n 6 -t 30 rsum
 if [[ ${rsum} == jdkskq ]];then
-    finailize && exit 0
-fi
+    echo "配置已生效"
+    exit 0
+else
 echo "rollback..."
-initialize
+if [[ ${OS} =~ ^Ubuntu$|^Debian$ ]];then
+	iptables-restore < /etc/iptables.up.rules
+        initialize
+	iptables-save > /etc/iptables.up.rules
+fi
+if [[ ${OS} == CentOS ]];then
+	if [[ $CentOS_RHEL_version == 7 ]];then
+		iptables-restore < /etc/iptables.up.rules
+		initialize
+		iptables-save > /etc/iptables.up.rules
+	else
+                initialize
+		/etc/init.d/iptables save
+		/etc/init.d/iptables restart
+	fi
+fi
+echo "配置已还原"
+exit 0
+fi
