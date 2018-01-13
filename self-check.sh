7b242d737e8339c94974fe7bcbe13c54806666cb
stack zhao <zyh001@users.noreply.github.com>
2018-01-13 10:46:03 +0800

    自动清日志，防止多次安装导致日志过大

--- self-check.sh
+++ self-check.sh
@@ -5,38 +5,39 @@ echo "##################################
       AR-B-P-B 自检系统
              V1.0 Alpha
 ##################################"
+rm -f report.json
 #List /usr/local
-echo "############Filelist of /usr/local" >> /root/report.json
+echo "############Filelist of /usr/local" >> report.json
 cd /usr/local
-ls >> /root/report.json
+ls >> report.json
 #List /usr/local/ssr-bash-python
-echo "############Filelist of /usr/local/SSR-Bash-Python" >> /root/report.json
+echo "############Filelist of /usr/local/SSR-Bash-Python" >> report.json
 cd /usr/local/SSR-Bash-Python
-ls >> /root/report.json
+ls >> report.json
 #List /usr/local/shadowsockr
-echo "############Filelist of /usr/local/shadowsockr" >> /root/report.json
+echo "############Filelist of /usr/local/shadowsockr" >> report.json
 cd /usr/local/shadowsocksr
-ls >> /root/report.json
-echo "############File test">>/root/report.json
+ls >> report.json
+echo "############File test" >> report.json
 #Check File Exist
 if [ ! -f "/usr/local/bin/ssr" ]; then
   echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> /root/report.json
+  echo "SSR Miss" >> report.json
   exit
 fi
 if [ ! -f "/usr/local/SSR-Bash-Python/server.sh" ]; then
   echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> /root/report.json
+  echo "SSR Miss" >> report.json
   exit
 fi
 if [ ! -f "/usr/local/shadowsocksr/stop.sh" ]; then
   echo "SSR主文件缺失，请确认服务器是否成功连接至Github"
-  echo "SSR Miss" >> /root/report.json
+  echo "SSR Miss" >> report.json
   exit
 fi
 
 #Firewall
 echo "############Firewall test" >> report.json
-iptables -L >> /root/report.json
+iptables -L >> report.json
 
 echo "检测完成，未发现严重问题，如仍有任何问题请提交report.json"
